9833874e28806b157274abf8ad190b6d5ea0edbb
rafaelalvesduarte2-lgtm <rafaelalvesduarte2@gmail.com>
2026-04-14 09:43:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 const projetos = [
   {
     nome: "Projeto 1",
-    descricao: "Meu primeiro site",
+    descricao: "Meu primeiro site em HTML e CSS",
     link: "#"
   },
   {
     nome: "Projeto 2",
-    descricao: "Projeto com JavaScript",
+    descricao: "Projeto com JavaScript dinâmico",
     link: "#"
   }
 ];
 
 const container = document.getElementById("lista-projetos");
-
-container.innerHTML = "";
 
 projetos.forEach(projeto => {
   const div = document.createElement("div");
@@ -22,7 +20,7 @@ projetos.forEach(projeto => {
   div.innerHTML = `
     <h3>${projeto.nome}</h3>
     <p>${projeto.descricao}</p>
-    <a href="${projeto.link}">Ver projeto</a>
+    <a href="${projeto.link}" target="_blank">Ver projeto</a>
   `;
 
   container.appendChild(div);
